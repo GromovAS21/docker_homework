30a324dc8e6371fd209e1d00034d7388a7ac156c
Anton Gromov <seed14112@yandex.ru>
2024-09-08 16:15:23 +0300

    Создание моделей User, Course, Lesson

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "users",
+    "materials"
 ]
 
 MIDDLEWARE = [
@@ -113,14 +114,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = "static/"
+STATICFILES_DIR = (BASE_DIR / "static")
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR / "media")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
--- materials/models.py
+++ materials/models.py
@@ -1,3 +1,70 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    """
+    Модель курса
+    """
+    title = models.CharField(
+        max_length=50,
+        verbose_name="Название курса",
+    )
+    description = models.TextField(
+        verbose_name="Описание курса",
+        blank=True,
+        null=True
+    )
+    image = models.ImageField(
+        upload_to='course/',
+        verbose_name="Превью курса",
+        blank=True,
+        null=True
+    )
+
+    class Meta:
+        verbose_name = "Курс",
+        verbose_name_plural = "Курсы"
+
+    def __str__(self):
+        return self.title
+
+
+class Lesson(models.Model):
+    """
+    Модель урока
+    """
+    course = models.ForeignKey(
+        Course,
+        verbose_name="Курс",
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL
+    )
+    title = models.CharField(
+        max_length=100,
+        verbose_name="Название урока"
+    )
+    description = models.TextField(
+        verbose_name="Описание урока"
+    )
+    image = models.ImageField(
+        upload_to='course/',
+        verbose_name="Превью урока",
+        blank=True,
+        null=True
+    )
+    url_video = models.CharField(
+        max_length=200,
+        verbose_name="Ссылка на видео",
+        blank=True,
+        null=True
+    )
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
+        ordering = ('id',)
+
+    def __str__(self):
+        return self.title
+
